a7a0bc16baf89e15f9a73bd167fc1619de01e6ad
Wentao Zheng <x1019575585@163.com>
2018-06-09 22:15:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,14 +8,21 @@ Created on Sat Jun  9 20:44:14 2018
 from yili import *
 import pandas as pd
 if __name__ == '__main__':
+    # read your own data
     data = pd.read_excel('data.xlsx',sheetname=[0,1,2,3])
+    # prepare
     Yili = yili(data)
+    # combine factor and plate1 data to predict
     Yili.combine_income()
+    # cobing factor and plate2 data to predict
     Yili.combine_price()
+    # plot and standardization
     Yili.plot_standardization()
+    # feature selection
     Yili.lasso(Yili.price)
     Yili.LinearRegression(Yili.price)
     Yili.Randomforest(Yili.price)
     Yili.Randomlasso(Yili.price)
+    # Lstm model to predict
     Yili.Lstm('income')
     Yili.Lstm('price')
